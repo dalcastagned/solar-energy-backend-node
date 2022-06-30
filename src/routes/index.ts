163782f2652016aedd749a9fa9c9614194c3fd45
Daniel Dalcastagne <dalcastagned@gmail.com>
2022-06-30 19:33:33 -0300

    feat: add cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,10 @@ import { Express } from 'express-serve-static-core';
 import { generationRoutes } from './GenerationsRoutes';
 import { plantRoutes } from './PlantsRoutes';
 import { userRoutes } from './UserRoutes';
+import cors from 'cors';
 
 const routes = (app: Express) => {
+  app.use(cors());
   app.use(express.json(), generationRoutes, plantRoutes, userRoutes);
 };
 
